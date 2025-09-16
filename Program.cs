342e682084f5c7d4c74b0e01f1220130f2ee1e91
Liam Mcdougall <000878229@mohawkcollege.ca>
2025-09-16 16:36:24 -0400

    Designed a opening screen, and choice options.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace mcdougall_liam_a1_tba
+﻿/* Name: Liam McDougall, 000878229
+ * Date: 2025/09/16
+ * Purpose: A text based adventure game
+ *
+ */
+namespace mcdougall_liam_a1_tba
 {
     internal class Program
     {
+        /*Variables for item 1*/
+        string name1;
+        int value1, rarity1, demand1, condition1;
+
+        /*Variables for item 2*/
+        string name2;
+        int value2, rarity2, demand2, condition2;
+
+        /*Variables for item 3*/
+        string name3;
+        int value3 = 0, rarity3, demand3, condition3;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("GAME TITLE TBA. \nPress the 'enter' key to continue. \nCreated by Liam McDougall.");
+            string choice = Console.ReadLine();
+            Console.WriteLine("Your father walks into the room. He places 3 item on the desk. ");
+
+            do
+            {
+                if (choice == "1"){
+                
+                }
+            } while (choice != "");
+
         }
     }
 }
