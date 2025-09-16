314b2e6ea4bcb8a591187cbe3526d34590835d48
Liam Mcdougall <000878229@mohawkcollege.ca>
2025-09-16 15:22:02 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace mcdougall_liam_a1_tba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
